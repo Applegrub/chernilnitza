5ae030b37416ea00032d419aa0f2a0e0b7944620
Алешенька <alexey@Alesenkas-MacBook-Pro.local>
2020-05-10 17:16:07 +0300

    fuck you matherfucker :ambulance: :fire:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Root = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+width: 100vw;
+background: url("https://i.pinimg.com/originals/e7/42/80/e7428019f4dc3be767ee4d481d7c3d55.jpg") center no-repeat;
+background-size: cover;
+
+`
+
+const Title = styled.div`
+  font-size: 90px;
+  color: cornflowerblue;
+  font-family: Roboto;
+  font-weight: 900;
+  padding: 60px;
+  background-color: white;
+  opacity: .9;
+  border-radius: 60px;
+`
+
+export default class App extends React.Component {
+
+    render() {
+        return <Root>
+            <Title>
+                ПОШЕЛ НАХУЙ
+            </Title>
+        </Root>
+    }
+
 }
-
-export default App;
