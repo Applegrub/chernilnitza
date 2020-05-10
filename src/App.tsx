1f27c9f8cc183d97cc9216290920ef3995388ae8
Алешенька <alexey@Alesenkas-MacBook-Pro.local>
2020-05-10 17:35:09 +0300

    something

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
 import React from 'react';
 import styled from "@emotion/styled";
+import Head from "./Components/Head";
 
 const Root = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 100vh;
-width: 100vw;
-background: url("https://i.pinimg.com/originals/e7/42/80/e7428019f4dc3be767ee4d481d7c3d55.jpg") center no-repeat;
-background-size: cover;
-
-`
-
-const Title = styled.div`
-  font-size: 90px;
-  color: cornflowerblue;
-  font-family: Roboto;
-  font-weight: 900;
-  padding: 60px;
-  background-color: white;
-  opacity: .9;
-  border-radius: 60px;
 `
 
 export default class App extends React.Component {
 
     render() {
         return <Root>
-            <Title>
-                ПОШЕЛ НАХУЙ
-            </Title>
+            <Head/>
         </Root>
     }
 
