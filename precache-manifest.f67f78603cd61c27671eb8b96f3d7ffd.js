629b03a32101156b9424cfc51377b20a9a1b10a1
Applegrub <sidyukovmichail@list.ru>
2020-06-09 14:36:19 +0300

    Updates

---
+++ precache-manifest.f67f78603cd61c27671eb8b96f3d7ffd.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f544040876a0bbd21cc607469387d50",
+    "url": "/chernilnitza/index.html"
+  },
+  {
+    "revision": "4f90fb144f162b172725",
+    "url": "/chernilnitza/static/js/2.b4b7c8b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chernilnitza/static/js/2.b4b7c8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3db0ce9654e1a116973",
+    "url": "/chernilnitza/static/js/main.0fa69e34.chunk.js"
+  },
+  {
+    "revision": "9576fb28d8b5f44588e4",
+    "url": "/chernilnitza/static/js/runtime-main.abdc1920.js"
+  },
+  {
+    "revision": "946eabc09cc71af47ab530d926537ab5",
+    "url": "/chernilnitza/static/media/BANNER1.946eabc0.png"
+  },
+  {
+    "revision": "e0479cefef748b2ac32ea77fc8c9f1d4",
+    "url": "/chernilnitza/static/media/BANNER2.e0479cef.png"
+  },
+  {
+    "revision": "6826453b5fba2b327050214f323d07c8",
+    "url": "/chernilnitza/static/media/BANNER3.6826453b.png"
+  },
+  {
+    "revision": "407f253785726cc354a8c87498631caa",
+    "url": "/chernilnitza/static/media/IG.407f2537.svg"
+  },
+  {
+    "revision": "bd36ab9e67f84ee75613432803c858a4",
+    "url": "/chernilnitza/static/media/Inks.bd36ab9e.svg"
+  },
+  {
+    "revision": "b607478e0ed978e2655976f7f3fc49cb",
+    "url": "/chernilnitza/static/media/Post1.b607478e.svg"
+  },
+  {
+    "revision": "7f7f2d05f08eafe1f65a5a2a3cb6cd64",
+    "url": "/chernilnitza/static/media/Post2.7f7f2d05.svg"
+  },
+  {
+    "revision": "94977b0aff48fd0d332890aee245dc63",
+    "url": "/chernilnitza/static/media/Post3.94977b0a.svg"
+  },
+  {
+    "revision": "d57c7d39874f1e845075204df94669a3",
+    "url": "/chernilnitza/static/media/cart.d57c7d39.svg"
+  },
+  {
+    "revision": "09f65ae3566a5eb795d2fec421b91567",
+    "url": "/chernilnitza/static/media/facebook.09f65ae3.svg"
+  },
+  {
+    "revision": "5342a4961c1dc08b4b05464f376eb3a0",
+    "url": "/chernilnitza/static/media/faq.5342a496.svg"
+  },
+  {
+    "revision": "588f42d553a193abcac3db9375aa999a",
+    "url": "/chernilnitza/static/media/inkFooter.588f42d5.svg"
+  },
+  {
+    "revision": "59dede6e714d14830649b4072ac9ac67",
+    "url": "/chernilnitza/static/media/instTree.59dede6e.svg"
+  },
+  {
+    "revision": "a787473caa327044a56fece144ea6427",
+    "url": "/chernilnitza/static/media/jesus1.a787473c.png"
+  },
+  {
+    "revision": "154b7480fd981f7fc2391aa343bbbc0e",
+    "url": "/chernilnitza/static/media/jesus2.154b7480.png"
+  },
+  {
+    "revision": "487ce16d1de56a6024ec57a1bda94dd9",
+    "url": "/chernilnitza/static/media/jesus3.487ce16d.png"
+  },
+  {
+    "revision": "be94ee563d76aa6cfbb772c577d1af46",
+    "url": "/chernilnitza/static/media/logo.be94ee56.svg"
+  },
+  {
+    "revision": "888e07a81b9d6fd90cf1b30a340e447d",
+    "url": "/chernilnitza/static/media/logoFooter.888e07a8.svg"
+  },
+  {
+    "revision": "604340f7cd798a15e00dd49ceffa9ae6",
+    "url": "/chernilnitza/static/media/oldAge1.604340f7.png"
+  },
+  {
+    "revision": "ad9d9220921325bf003d3b2607aec510",
+    "url": "/chernilnitza/static/media/oldAge2.ad9d9220.png"
+  },
+  {
+    "revision": "da29478baa4e5dec90e730d29944ae17",
+    "url": "/chernilnitza/static/media/oldAge3.da29478b.png"
+  },
+  {
+    "revision": "d8013e8e685517711130b8d85f44e640",
+    "url": "/chernilnitza/static/media/oldAgeExclusive.d8013e8e.png"
+  },
+  {
+    "revision": "86f9dd2db4daeced61f73f8210e242e6",
+    "url": "/chernilnitza/static/media/oldAgeWithPrise.86f9dd2d.png"
+  },
+  {
+    "revision": "7d117773ad161095f2c6dda248b4ff3c",
+    "url": "/chernilnitza/static/media/printerest.7d117773.svg"
+  },
+  {
+    "revision": "16025e0b4eb09f44a63c3e79c6f33259",
+    "url": "/chernilnitza/static/media/telegram.16025e0b.svg"
+  },
+  {
+    "revision": "9b5196703153fe1ad1235c46ec067349",
+    "url": "/chernilnitza/static/media/twiter.9b519670.svg"
+  },
+  {
+    "revision": "5a8b751634a6688b8f282f45985d253f",
+    "url": "/chernilnitza/static/media/vk.5a8b7516.svg"
+  },
+  {
+    "revision": "b9dac074e6e6dae8835650edccdf1fdd",
+    "url": "/chernilnitza/static/media/withLove1.b9dac074.png"
+  },
+  {
+    "revision": "78432a3b6a257b92fb12d9f640ebd5e0",
+    "url": "/chernilnitza/static/media/withLove2.78432a3b.png"
+  },
+  {
+    "revision": "f13b9fce57d147688263ae8c6363ea45",
+    "url": "/chernilnitza/static/media/withLove3.f13b9fce.png"
+  },
+  {
+    "revision": "89814b7264f1eeadcf1b97e20b71b773",
+    "url": "/chernilnitza/static/media/youAreGood.89814b72.png"
+  }
+]);
