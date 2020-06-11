2eca321e5223bf272992ccd72cfcbbb998176fa5
Veserion <48389713+Veserion@users.noreply.github.com>
2020-06-11 21:10:49 +0300

    background did fixed

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,16 +1,20 @@
+/**@jsx jsx*/
 import React from "react";
 import styled from "@emotion/styled";
+import { css, jsx } from '@emotion/core'
 import ink from '../../assets/images/inkFooter.svg'
 import logo from '../../assets/images/logoFooter.svg'
 import Menu from "./Menu";
 import Icon from "../SocialNetBar";
+import { useWindowDimensions } from '../../utils/dimensions'
 
-const Root = styled.div`
+const Root= styled.div`
 width: 100%;
 display: flex;
 height: 400px;
 background: url(${ink}) no-repeat left;
 padding-top: 659px;
+z-index: -1;
 `
 const Logo = styled.div`
 margin-left: 23%;
@@ -24,16 +28,18 @@ const SocNetBar = styled.div`
 display: flex;
 justify-content: flex-end;
 margin: 20px -10px -10px -10px;
+z-index: 100;
  & > *{
  margin: 10px;
  }
 `
-const Footer: React.FunctionComponent = () =>{
-    return <Root>
-        <Logo><img src={logo} alt="Logo"/></Logo>
+const Footer: React.FunctionComponent = () => {
+    const { width } = useWindowDimensions();
+    return <Root css={css`background-position: ${-1600 + width}px;`}>
+        <Logo><img src={logo} alt="Logo" /></Logo>
         <MenuAndSocNetBar>
-            <Menu/>
-            <SocNetBar>
+            <Menu />
+            <SocNetBar >
                 <Icon icon={"facebook"} />
                 <Icon icon={"twitter"} />
                 <Icon icon={"printerest"} />
